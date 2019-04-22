0f7a2f4c34b96c048e9058a21276c0b3d7322e44
LucreciaLin <lucrecialin@LucreciaLinde-MacBook-Pro.local>
2019-04-22 10:43:37 +0800

    update comments

--- stack.c
+++ stack.c
@@ -11,15 +11,15 @@
 typedef unsigned int UINT32;    //  這裡標示，可作為：1.縮寫用、2.方便移植程式用，因unsigned int包含16或32位元，這裡明訂用32位元。
 
 int main() {
-    char oriAry[101];   // 先分配100 * sizeof(char) 個bytes。
-    char stack[101];    // 先分配100 * sizeof(char) 個bytes。
+    char oriAry[101];   // 先分配101 * sizeof(char) 個bytes。
+    char stack[101];    // 先分配101 * sizeof(char) 個bytes。
     int i;
     int top = 0;        // 因stack只在一端出或入資料，這裡命名那一端為「top」。
     int startComparingIdx;  // 是另一半迴文資料用以「比對是否為迴文的起始位置」。
     
     fgets(oriAry, sizeof(oriAry), stdin);   // 用以取得使用者輸入的一串文字，會包含最後按下的Enter字元。注意：gets()已deprecated，改用fgets()較安全。
     
-    int len = (UINT32) strlen(oriAry) - 1;  // 因為strlen出來是「unsigned long」，所以要轉型。而這裡「選用unsigned int為因結果一定大於0」。另外，「-1是為了扣除之前的Enter字元」。
+    int len = (UINT32) strlen(oriAry) - 1;  // 因為strlen回傳型態為「unsigned long」，所以要轉型。而這裡「選用unsigned int為因結果一定大於0」。另外，「-1是為了扣除之前的Enter字元」。
     int beforeMidIdx = (len / 2) - 1;       // 因爲「迴文如果取中點後，其左右會是對稱關係」。注意：C語言中，「整數相除，結果會無條件捨去」，e.g. 當迴文為「xyzyx」時，5/2 = 2，得到中點索引值為2，而再-1是為了之後取不要放入中點位置的資料。e.g. 當迴文為「anna」，4/2 = 2，得到中點索引值為2，而再-1是為了之後取不要放入中點位置的資料。
     if (len % 2 == 0)
     {
